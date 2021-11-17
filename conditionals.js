31198f91a7897d88cce451ad7223762c057a166a
ShirelleW <shirellewinecoffm@gmail.com>
2021-11-17 15:39:58 -0500

    completed excercises in conditionals and loops

---
+++ conditionals.js
@@ -0,0 +1,30 @@
+// let integer = -1;
+
+// if (integer < 0){
+//     console.log("Negative: " + integer)
+// } else{
+//     console.log("Positive: " + integer)
+// }
+
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("Access Granted!")
+// }else{
+//     console.log("Access denied")
+// }
+
+let grade = 69;
+
+if (grade >= 90){
+    console.log("You have an A")
+} else if (grade >= 80 && grade < 90){
+    console.log("You have a B")
+} else if (grade >= 70 && grade < 80){
+    console.log("You have a C")
+} else if (grade >= 55 && grade < 70){
+    console.log("You have a D")
+} else {
+    console.log("You have an F")
+}
---
+++ loops.js
@@ -0,0 +1,17 @@
+
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+// for (let i = 1; i < 10; i = i+2){
+//     console.log(i)
+// }
+
+for (let i = 2; i < 11; i = i + 2){
+    console.log(i)
+}
